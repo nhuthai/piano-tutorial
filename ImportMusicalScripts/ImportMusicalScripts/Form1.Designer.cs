0f97a331f80746353447f33ffa20a41cac5689f8
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2017-09-16 23:29:38 +0200

    modify import database

--- ImportMusicalScripts/ImportMusicalScripts/Form1.Designer.cs
+++ ImportMusicalScripts/ImportMusicalScripts/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -46,6 +48,7 @@
             // 
             // button2
             // 
+            this.button2.Enabled = false;
             this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
@@ -60,25 +63,47 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Location = new System.Drawing.Point(12, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(260, 130);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Choose excel file, please!!!";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 48);
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(141, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Value = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(57, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Open folder containing Json file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 284);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -88,6 +113,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ImportMusicalScripts/ImportMusicalScripts/Form1.cs
+++ ImportMusicalScripts/ImportMusicalScripts/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -11,6 +12,7 @@ using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
 using LitJson;
 using System.IO;
+using System.Diagnostics;
 
 namespace ImportMusicalScripts
 {
@@ -23,24 +25,37 @@ namespace ImportMusicalScripts
             FileInfo file = new FileInfo(Application.ExecutablePath);
             _path = file.Directory.FullName;
 
-            excel_file = _path;
+            excel_file = Path.Combine(_path, "Sample of Musical Scripts.xlsx");
 
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
-            excel_file = openFileDialog1.FileName;
+            openFileDialog1.Filter = "Excel files (*.xlsx) | *.xlsx";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                excel_file = openFileDialog1.FileName;
+                this.button2.Enabled = true;
+                this.label1.Text = excel_file + " has been chosen. Please press \"Create Json\" button!!!";
+                this.button3.Enabled = false;
+                //this.button3.Text = "Open " + _path;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            getExcelFile();
+            Thread tid1 = new Thread(new ThreadStart(getExcelFile));
+            tid1.Start();
         }
 
         public void getExcelFile()
         {
+            if (!File.Exists(excel_file))
+            {
+                MessageBox.Show("File " + excel_file + " doesn't exist!");
+                return;
+            }
             //Create COM Objects. Create a COM object for everything that is referenced
             Excel.Application xlApp = new Excel.Application();
             Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@excel_file);
@@ -48,37 +63,74 @@ namespace ImportMusicalScripts
             Excel.Range xlRange;
             string myResult = "";
             SongList PlayList = new SongList();
-
-            for (int i_sheet = 1; i_sheet <= xlWorkbook.Sheets.Count; i_sheet++)
+            try
             {
-                xlWorksheet = xlWorkbook.Sheets[i_sheet];
-                xlRange = xlWorksheet.UsedRange;
-
-                int rowCount = xlRange.Rows.Count;
-                int colCount = xlRange.Columns.Count;
-
-                //iterate over the rows and columns and print to the console as it appears in the file
-                //excel is not zero based!!
-
-                Song aSong = new Song(xlWorksheet.Name);
-
-                for (int i = 2; i <= rowCount; i++)
+                this.progressBar1.Maximum = xlWorkbook.Sheets.Count + 2;
+                this.SetProgressBarVisible(this.progressBar1, true);
+                for (int i_sheet = 1; i_sheet <= 2; i_sheet++)
                 {
-                    List<int> aDuration = new List<int>();
-                    for (int j = 1; j <= colCount; j++)
+                    xlWorksheet = xlWorkbook.Sheets[i_sheet];
+                    xlRange = xlWorksheet.UsedRange;
+                    try
                     {
-                        if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        this.SetInforStatus(this.label1, "Reading " + xlWorksheet.Name + " ...");
+                        this.SetProgressBarStep(this.progressBar1);
+                        int rowCount = xlRange.Rows.Count;
+                        int colCount = xlRange.Columns.Count;
+
+                        //iterate over the rows and columns and print to the console as it appears in the file
+                        //excel is not zero based!!
+
+                        Song aSong = new Song(xlWorksheet.Name);
+
+                        for (int i = 2; i <= rowCount; i++)
                         {
-                            int tmp_number = 0;
-                            Int32.TryParse(xlRange.Cells[i, j].Value2.ToString(), out tmp_number);
-                            aDuration.Add(tmp_number);
+                            List<int> aDuration = new List<int>();
+                            for (int j = 1; j <= colCount; j++)
+                            {
+                                if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                                {
+                                    int tmp_number = 0;
+                                    Int32.TryParse(xlRange.Cells[i, j].Value2.ToString(), out tmp_number);
+                                    aDuration.Add(tmp_number);
+                                }
+                            }
+                            aSong.addDuration(aDuration);
                         }
+                        this.SetInforStatus(this.label1, "Read " + xlWorksheet.Name + "!!!");
+
+                        PlayList.addSong(aSong);
                     }
-                    aSong.addDuration(aDuration);
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.ToString());
+                    }
+                    finally
+                    {
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+
+                        //release com objects to fully kill excel process from running in the background
+                        Marshal.ReleaseComObject(xlRange);
+                        Marshal.ReleaseComObject(xlWorksheet);
+                    }
                 }
 
-                PlayList.addSong(aSong);
+                this.SetInforStatus(this.label1, "Converting into Json object ...");
+                myResult = JsonMapper.ToJson(PlayList);
 
+                string filePath = Path.Combine(_path, "melody.json");
+                this.SetInforStatus(this.label1, "Saving to " + filePath + " ...");
+                File.WriteAllText(filePath, myResult);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                this.SetInforStatus(this.label1, "Closing " + excel_file + " ...");
+                //cleanup
                 GC.Collect();
                 GC.WaitForPendingFinalizers();
 
@@ -86,38 +138,79 @@ namespace ImportMusicalScripts
                 //  never use two dots, all COM objects must be referenced and released individually
                 //  ex: [somthing].[something].[something] is bad
 
-                //release com objects to fully kill excel process from running in the background
-                Marshal.ReleaseComObject(xlRange);
-                Marshal.ReleaseComObject(xlWorksheet);
+                //close and release
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+
+                //quit and release
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+                this.SetProgressBarStep(this.progressBar1);
+                this.SetProgressBarVisible(this.progressBar1, false);
+                this.SetInforStatus(this.label1, "Done!!! Database file \"melody.json\" has been saved at " + _path);
+                this.Setbutton3Enabled(this.button3, true);
             }
-            myResult = JsonMapper.ToJson(PlayList);
+        }
 
-            string filePath = Path.Combine(_path, "melody.json");
+        public delegate void ProgressBarVisibleDelegate(ProgressBar pBar, bool visible);
 
-            try
+        public void SetProgressBarVisible(ProgressBar pBar, bool visible)
+        {
+            if (pBar.InvokeRequired)
             {
-                File.WriteAllText(filePath, myResult);
+                pBar.Invoke(new ProgressBarVisibleDelegate(SetProgressBarVisible), new object[] { pBar, visible });
             }
-            catch(Exception ex)
+            else
             {
-                MessageBox.Show(filePath + " " + ex.ToString());
+                pBar.Visible = visible;
             }
+        }
 
-            //cleanup
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
+        public delegate void ProgressBarStepDelegate(ProgressBar pBar);
 
-            //rule of thumb for releasing com objects:
-            //  never use two dots, all COM objects must be referenced and released individually
-            //  ex: [somthing].[something].[something] is bad
+        public void SetProgressBarStep(ProgressBar pBar)
+        {
+            if (pBar.InvokeRequired)
+            {
+                pBar.Invoke(new ProgressBarStepDelegate(SetProgressBarStep), new object[] { pBar });
+            }
+            else
+            {
+                pBar.PerformStep();
+            }
+        }
 
-            //close and release
-            xlWorkbook.Close();
-            Marshal.ReleaseComObject(xlWorkbook);
+        public delegate void InforStatusDelegate(Label lbl, string mes);
 
-            //quit and release
-            xlApp.Quit();
-            Marshal.ReleaseComObject(xlApp);
+        public void SetInforStatus(Label lbl, string mes)
+        {
+            if (lbl.InvokeRequired)
+            {
+                lbl.Invoke(new InforStatusDelegate(SetInforStatus), new object[] { lbl, mes });
+            }
+            else
+            {
+                lbl.Text = mes;
+            }
+        }
+
+        public delegate void button3EnabledDelegate(Button btn, bool enabled);
+
+        public void Setbutton3Enabled(Button btn, bool enabled)
+        {
+            if (btn.InvokeRequired)
+            {
+                btn.Invoke(new button3EnabledDelegate(Setbutton3Enabled), new object[] { btn, enabled });
+            }
+            else
+            {
+                btn.Enabled = enabled;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", @_path);
         }
     }
 }
